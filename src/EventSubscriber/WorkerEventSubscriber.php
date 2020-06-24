f6e45c13870af7e2df1c26e065acb08019d1ec78
Jakob Schumann <114239+j-schumann@users.noreply.github.com>
2020-06-24 14:15:58 +0200

    upd: worker event subscriber

--- src/EventSubscriber/WorkerEventSubscriber.php
+++ src/EventSubscriber/WorkerEventSubscriber.php
@@ -4,16 +4,25 @@ declare(strict_types=1);
 
 namespace App\EventSubscriber;
 
-use Psr\Log\LoggerInterface;
+use Monolog\Handler\BufferHandler;
+use Monolog\Processor\UidProcessor;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent;
+use Symfony\Component\Messenger\Event\WorkerMessageHandledEvent;
 use Symfony\Component\Messenger\Event\WorkerMessageReceivedEvent;
-use Symfony\Contracts\Service\ServiceSubscriberInterface;
-use Symfony\Contracts\Service\ServiceSubscriberTrait;
 
+/**
+ * @todo how can we inject the handlers for all channels at once, so that we can
+ * flush them all at once, without registering this subscriber multiple times?
+ * @todo How can we reset the UidProcessor only once without depending on the
+ * handler name?
+ */
 class WorkerEventSubscriber
-    implements EventSubscriberInterface, ServiceSubscriberInterface
+    implements EventSubscriberInterface, LoggerAwareInterface
 {
-    use ServiceSubscriberTrait;
+    use LoggerAwareTrait;
 
     /**
      * @inheritDoc
@@ -21,19 +30,54 @@ class WorkerEventSubscriber
     public static function getSubscribedEvents()
     {
         return [
-            WorkerMessageReceivedEvent::class => 'onMessageReceived',
+            // after a new message is received, before handling: reset the UID
+            // added to the logs so we can separate log entries by handled
+            // message, like with indivual HTTP requests
+            WorkerMessageReceivedEvent::class => 'resetLogger',
+
+            // after the handling succeeded or failed: flush all BufferHandlers
+            // so we can immediatly see log entries, not just when the worker
+            // exits / the buffer limit is reached
+            WorkerMessageHandledEvent::class => 'flushLogger',
+            WorkerMessageFailedEvent::class => 'flushLogger',
         ];
     }
 
-    public function onMessageReceived(WorkerMessageReceivedEvent $event)
+    /**
+     * For each received message, reset the UidProcessor to generate a new
+     * identifier. But do this only once for the "app" channel, the same
+     * processor is used for all handlers, we don't want to reset the ID twice
+     * and possibly split messages between the IDs.
+     */
+    public function resetLogger()
     {
-        $this->logger()->debug('resetting logger');
-        $this->logger()->reset();
-        $this->logger()->debug('post reset');
+        if ($this->logger->getName() !== 'app') {
+            return;
+        }
+
+        // don't simply reset() the logger itself, we don't need to flush
+        // messages already
+        foreach($this->logger->getProcessors() as $processor) {
+            if ($processor instanceof UidProcessor) {
+                $processor->reset();
+            }
+        }
     }
 
-    protected function logger(): LoggerInterface
+    /**
+     * After a message was processed flash any buffer handlers, we don't want
+     * to wait till the next message to see the logs.         
+     */    
+    public function flushLogger()
     {
-        return $this->container->get(__METHOD__);
+       // don't simply reset() the logger itself, this would cause the UID  to
+       // change too, which would separate log entries coming after this event
+       // (low priority, but still belonging to this message, e.g. messenger ACK 
+       // to transport). 
+       foreach($this->logger->getHandlers() as $handler) {
+          if ($handler instanceof BufferHandler) {
+              $handler->flush();
+          }
+       }
     }
 }
--- src/MessageHandler/GeneratePdfMessageHandler.php
+++ src/MessageHandler/GeneratePdfMessageHandler.php
@@ -24,8 +24,6 @@ class GeneratePdfMessageHandler implements MessageHandlerInterface, ServiceSubsc
      */
     public function __invoke(GeneratePdfMessage $message)
     {
-	$this->logger()->debug('recieving mess');
-
         $tex = $message->getLatex();
 
         $wrapper = new Wrapper();
